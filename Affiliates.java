068af36866af071a08869ded6ceac2331514d2e4
yefer cordoba <yexcordoba@outlook.com>
2024-10-11 12:32:03 -0500

    the affiliate class is optimized, extracting methods from other classes.

--- Affiliates.java
+++ Affiliates.java
@@ -1,42 +1,37 @@
-import java.util.HashSet;
 import java.util.Scanner;
-/**
- *
- * @author yanca
- */
+import java.util.HashSet;
 
 public class Affiliates extends Member {
     private String lastName;
     private HashSet<String> namesOfAssociates;
- 
+
     public Affiliates(String lastName, String name, String id, int availableFunds, String subscription, int pendingInvoices) {
-        super(name, id, availableFunds, subscription, pendingInvoices);
+        super(name, id);
         this.lastName = lastName;
         this.namesOfAssociates = new HashSet<>();
     }
-   
+    
     public Affiliates() {
-        super(" ", " ");
+        super(" ", " "); 
         this.lastName = "";
-        this.namesOfAssociates = new HashSet<>();
+        this.namesOfAssociates = new HashSet<>(); 
     }
+
     public String getLastName() {
         return lastName;
     }
- 
- 
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-   
- 
+
     public void listOfPeople(Scanner sc) {
         System.out.println("Enter the names of the associates (max. 10):");
         while (namesOfAssociates.size() < 10) {
             System.out.println("Enter a name (or type 'exit' to finish):");
             String nickname = sc.nextLine();
             if (nickname.equalsIgnoreCase("exit")) {
-                break;
+                break; 
             }
             if (namesOfAssociates.contains(nickname)) {
                 System.out.println("The name is already registered. Please enter another.");
@@ -46,11 +41,13 @@ public class Affiliates extends Member {
         }
         System.out.println("Registered users: " + namesOfAssociates);
     }
+
     public void showInfoAffiliates() {
         System.out.println("===== Affiliates Info =====");
         System.out.println("Registered Associates: " + (namesOfAssociates.isEmpty() ? "No associates registered" : namesOfAssociates));
     }
 }
+
          
          
     
